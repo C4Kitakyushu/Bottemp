f00091ef263442ea0ad642d44409ae5a0b3438c8
C4Kitakyushu <156558807+C4Kitakyushu@users.noreply.github.com>
2025-04-08 18:01:38 +0800

    Update fbcoverv5.js

--- cmds/fbcoverv5.js
+++ cmds/fbcoverv5.js
@@ -30,7 +30,7 @@ module.exports = {
     }
 
     const [name, id, subname, color] = details.map(encodeURIComponent);
-    const apiUrl = `https://api.zetsu.xyz/canvas/fbcoverv5?name=${name}&id=${id}&subname=${subname}&color=${color}`;
+    const apiUrl = `http://87.106.100.187:6312/canvas/fbcoverv5?name=${name}&id=${id}&subname=${subname}&color=${color}`;
     const filePath = path.join(__dirname, `fbcoverv5_${Date.now()}.jpg`);
 
     try {
